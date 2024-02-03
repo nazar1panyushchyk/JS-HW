55bb4a25b18db484c71544b5cae1dfd23910c3f8
Nazar_Panyushchyk <npanusik@gmail.com>
2024-02-03 17:08:14 +0200

    add

---
+++ script.js
@@ -0,0 +1,8 @@
+const name = prompt("Enter your name.");
+console.log('name');
+
+const warning = alert("Action is dangerous. Confirm the action by clicking confirm.")
+console.log('warning');
+
+const age = confirm("Are you 18?");
+console.log('age');
